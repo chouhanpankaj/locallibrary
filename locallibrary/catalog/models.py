ce1025352f2df43381fc61a477eb826451191efb
Pankaj Chouhan <chouhanpankaj711@gmail.com>
2020-07-22 17:20:56 +0530

    Models registerd to admin

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -20,7 +20,7 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    
+    Language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     def __str__(self):
         #String for representing the Model object.
         return self.title
@@ -82,6 +82,6 @@ class Language(models.Model):
     help_text="Enter the book's natural language (e.g. english, hindi)"
     )
 
-    def __str__():
+    def __str__(self):
         # String for representing the model object.
         return self.name
