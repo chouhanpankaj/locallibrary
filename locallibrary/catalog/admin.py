d95191710dd17df311addb0692848dc28115b241
Pankaj Chouhan <chouhanpankaj711@gmail.com>
2020-07-22 18:15:21 +0530

    added Modeladmin classes

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)  # works same as admin.site.resgister(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
