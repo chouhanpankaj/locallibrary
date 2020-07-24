98857ab7073365ade824c0b37925896984c88ee5
Pankaj Chouhan <chouhanpankaj711@gmail.com>
2020-07-24 19:00:08 +0530

    Book list page added

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def  index(request):
     # view function for home page of site.
@@ -26,3 +27,6 @@ def  index(request):
 
     # Render the HTML template 'index.html' with the data in the context variable.
     return render(request, 'index.html', context = context)
+
+class BookListView(generic.ListView):
+    model = Book 
